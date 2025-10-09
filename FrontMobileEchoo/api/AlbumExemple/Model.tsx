0478c7f9a85b52db9b6f64ccc2ea9761c745e165
TitiLeGrand <thibaultcallerand@gmail.com>
2025-10-09 15:34:57 +0200

    feat : Ajout exemple pour les requête

---
+++ FrontMobileEchoo/api/AlbumExemple/Model.tsx
@@ -0,0 +1,13 @@
+
+export interface Album {
+  id: number
+  title: string
+  artistId: number
+  year: number
+}
+
+export interface NewAlbum {
+  title: string
+  artistId: number
+  year: number
+}
--- FrontMobileEchoo/hook/globalHook.tsx
+++ FrontMobileEchoo/hook/globalHook.tsx
@@ -1,5 +1,7 @@
 import { create } from 'zustand'
+import { getAllAlbums } from '../api/AlbumExemple/Request'
 
+const API_BASE = 'https://api.example.com'
 
 // Iterface a alimenter avec le store (elle permet de typer les variables et fonctions du store)
 interface AppState {
@@ -20,9 +22,29 @@ const useAppStore = create<AppState>((set) => ({
     // #endregion
 
     // #region fonctions
-    setExempleVar: (value: number) => set(() => ({ exempleVar: value })),
+    setExempleVar: (value: number) => set({ exempleVar: value }),
     IncrementExempleVar2: () => set((state) => ({ exempleVar2: state.exempleVar2 + 1 })),
+
+    addAlbumExemple: (titre: string, artisteId: number) => {
+
+    },
+    getAlbumsByArtisteId: (artisteId: number) => {
+
+    },
+    getAllAlbums: async () => {
+        try {
+            const val = await getAllAlbums() 
+            set({ exempleVar: 1 }) 
+            set({ exempleVar: 2 }) 
+            return val          
+        } catch (error) {
+            console.error('Erreur dans getAllAlbums:', error)
+            return []
+        }
+    },
+
     // #endregion
-    
+
 }))
 export default useAppStore
+
