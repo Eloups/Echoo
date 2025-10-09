78e5e5b271342ebcfdcfa9b4bdf66dce4324b31c
Eloups <121400663+Eloups@users.noreply.github.com>
2025-10-09 15:44:18 +0200

    docs: ajout de commentaires - ajout de commentaires dans les tests de contrôleurs

--- apps/api/tests/unit/ControllerTest.php
+++ apps/api/tests/unit/ControllerTest.php
@@ -9,10 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ControllerTest extends TestCase
 {
+    /**
+     * Test du contrôleur de musiques
+     * @return void
+     */
     public function test_music_controller(): void
     {
+        // Création du contrôleur de musiques
         $musicController = new MusicController('list');
 
+        // Tests du contrôleur de musiques
         $this->assertEquals('list', $musicController->getAction());
         $this->assertEquals(new Response(json_encode(['code' => 200, 'message' => 'Réussi']), 200), $musicController->run());
     }
