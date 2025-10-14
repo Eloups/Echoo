0e8aad0f956dad82cfa78bfa1532133abf4e026a
Corentin SCHEER <156903441+CRT15@users.noreply.github.com>
2025-10-14 11:22:48 +0200

    docs: ajout des commentaires

--- apps/api/src/adapter/MusicDrivingAdapter.php
+++ apps/api/src/adapter/MusicDrivingAdapter.php
@@ -5,7 +5,15 @@ namespace Api\Adapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Classe pour l'adaptateur des musiques
+ */
 class MusicDrivingAdapter {
+    /**
+     * Récupération des données de la requête "liste des musiques" puis utilisation du bon service
+     * @param Request $request
+     * @return Response
+     */
     public function listMusics(Request $request): Response {
         return new Response(json_encode(['code' => 200, 'message' => 'Réussi']), 200);
     }
