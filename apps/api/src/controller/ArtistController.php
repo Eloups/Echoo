47d56f3a776900efd257346aa5101cc2ac85c662
Corentin SCHEER <156903441+CRT15@users.noreply.github.com>
2025-11-24 17:20:26 +0100

    feat: début feature

---
+++ apps/api/src/controller/ArtistController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Api\Controller;
+
+use Api\Adapter\MusicDrivingAdapter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+/**
+ * Controlleur des fonctions liées aux musiques
+ */
+class ArtistController implements ControllerInterface
+{
+    /**
+     * Action à lancer pour le contrôleur
+     * @var string
+     */
+    private string $action;
+
+    /**
+     * Récupérer les paramètres de la requête depuis le routeur
+     * @var array
+     */
+    private array $params = [];
+
+    /**
+     * Constructeur du contrôlleur de musiques
+     * @param string $action
+     */
+    public function __construct(string $action, array $params = [])
+    {
+        $this->action = $action;
+        $this->params = $params;
+    }
+
+    /**
+     * Lancement de l'action du contrôleur
+     * @throws ResourceNotFoundException si l'action n'existe pas
+     * @return Response
+     */
+    public function run(Request $request): Response
+    {
+        $adapter = new ArtistDrivingAdapter();
+        $idArtist = $this->params["id"];
+
+        return match ($this->action) {
+            'page' => $adapter->listMusics( $idArtist),
+            default => throw new ResourceNotFoundException(),
+        };
+    }
+
+    /**
+     * Accesseur de l'action du contrôleur
+     * @return string
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+}
--- apps/api/src/router/routes.php
+++ apps/api/src/router/routes.php
@@ -8,4 +8,6 @@ $routes = new RouteCollection();
 // On crée les routes ici
 $routes->add('music|list', new Route('/artist/{id}/musics', methods: 'GET'));
 
+$routes->add('artist|page', new Route('/artist/{id}/page', methods: 'GET'));
+
 return $routes;
