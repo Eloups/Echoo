0b3d609b851425394ebac3aed043736c4d8bb3bb
ThibaultCal <122479447+TitiLeGrand@users.noreply.github.com>
2025-12-01 09:02:48 +0100

    fix : ajout .env.exemple

--- auth-service/index.js
+++ auth-service/index.js
@@ -12,9 +12,11 @@ app.use(express.json())
 app.use(router)
 
 // Start le serveur publicement accessible par le tel (sinon il tape sur le localhost du tel et ça marche pas)
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Auth service running on 0.0.0.0:3000")
-})
-// app.listen(3000, () => {
-//     console.log("✅ Better Auth service running on http://localhost:3000")
+// app.listen(3000, "0.0.0.0", () => {
+//     console.log("Auth service running on 0.0.0.0:3000")
 // })
+
+// Avec le local 
+app.listen(8000, () => {
+    console.log("✅ Better Auth service running on http://localhost:8000")
+})
