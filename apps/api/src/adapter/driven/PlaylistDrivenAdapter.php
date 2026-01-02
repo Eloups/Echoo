b67ae816098c00fd0c6e86bec49903937f0e6eec
Corentin SCHEER <156903441+CRT15@users.noreply.github.com>
2026-01-02 11:08:05 +0100

    fix: conflits merge request

--- apps/api/src/adapter/driven/PlaylistDrivenAdapter.php
+++ apps/api/src/adapter/driven/PlaylistDrivenAdapter.php
@@ -70,6 +70,26 @@ class PlaylistDrivenAdapter implements PlaylistDrivenAdapterInterface
     }
 
     /**
+     * Méthode pour créer une nouvelle playlist
+     * @param int $id_library
+     * @param string $title
+     * @param bool $isPublic
+     * @param string $description
+     * @param string $cover_path
+     * @param array $musics
+     * @return void
+     */
+    public function addPlaylist(int $id_library, string $title, bool $isPublic, string $description, string $cover_path, array $musics)
+    {
+        $pgslserver = new PgsqlServer();
+
+        $pdo = $pgslserver->getConnection();
+        $request = new PgsqlPlaylistRequests($pdo);
+
+        $request->addPlaylist($id_library, $title, $isPublic, $description, $cover_path, $musics);
+    }
+
+    /**
      * Méthode pour ajouter une musique à une playlist
      * @param int $id_playlist
      * @param int $id_music
