c33bb1c2772b0bc4079e3ca3357562245d044cbf
Corentin SCHEER <156903441+CRT15@users.noreply.github.com>
2026-02-02 15:39:58 +0100

    feat: début tests e2e pour les musiques

---
+++ apps/api/tests/e2e/Api/MusicCest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Api;
+
+use Tests\Support\ApiTester;
+
+final class MusicCest
+{
+    /**
+     * Test de /music/{id}/coverPath
+     * @param ApiTester $I
+     * @return void
+     */
+    public function GetCoverPathProjectWithIdMusic(ApiTester $I): void
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+        $response = $I->sendGet('/music/10/coverPath');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson('{
+            "cover_path": "RadicalOptimism.jpg"
+        }');
+    }
+
+    /**
+     * Test de /musics/1/ratings
+     * @param ApiTester $I
+     * @return void
+     */
+    public function GetMusicRatings(ApiTester $I): void
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+        $response = $I->sendGet('/musics/1/ratings');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'ratings' => [
+                [
+                    'id' => 1,
+                    'rate' => 5,
+                    'createdAt' => '2025-10-03',
+                    'comment' => 'Comment 1',
+                    'user' => 1,
+                ],
+                [
+                    'id' => 16,
+                    'rate' => 4,
+                    'createdAt' => '2025-10-18',
+                    'comment' => 'Comment 16',
+                    'user' => 6,
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Test de /music/like
+     * @param ApiTester $I
+     * @return void
+     */
+    public function PostLikeMusic(ApiTester $I): void
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+
+        $I->sendPost(
+            '/music/like',
+            json_encode([
+                'id_user' => 5,
+                'id_music' => 11
+            ])
+        );
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'code' => 200,
+            'message' => 'Like ajouté avec succès'
+        ]);
+    }
+}
