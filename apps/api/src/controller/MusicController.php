73f1ceef1d8a065f5bcb44d0ba4ffa80210829d3
Eloups <121400663+Eloups@users.noreply.github.com>
2026-03-03 16:32:12 +0100

    feat: retrait de l'auth sur la route get image

--- apps/api/src/controller/MusicController.php
+++ apps/api/src/controller/MusicController.php
@@ -3,6 +3,7 @@
 namespace Api\Controller;
 
 use Api\Adapter\MusicDrivingAdapter;
+use Api\Utils\AuthUtils;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
@@ -41,6 +42,9 @@ class MusicController implements ControllerInterface
      */
     public function run(Request $request): Response
     {
+        // Authentification
+        AuthUtils::authenticate($request);
+
         $adapter = new MusicDrivingAdapter();
         $response = match ($this->action) {
             'list' => $adapter->listMusics($this->params["id"]),
